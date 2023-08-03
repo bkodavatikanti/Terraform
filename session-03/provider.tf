4a2876e2848734b966526416e081bb419428b462
bkodavatikanti <kumar.mokshi@gmail.com>
2023-08-03 14:22:06 +0530

    adding s3 as remote backend

--- session-03/provider.tf
+++ session-03/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.64.0"
     }
   }
+  backend "s3" {
+    bucket = "statelock461"
+    key    = "timing"
+    region = "ap-south-1"
+    dynamodb_table = "slterraform461"
+  }
 }
 
 # Configure the AWS Provider
